74b8b4bd859f2bd957ecb8aac1d1e283b0b54052
Brian Jordan <bcjordan@gmail.com>
2012-05-16 23:13:07 -0400

    thru ex5

---
+++ ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  puts("Hello world.");
+  puts("This is the last time I will contact you.");
+  puts("In forever...");
+  puts("This\nis\nanother\nline okay?");
+  return 0;
+}
