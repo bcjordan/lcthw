5ed01f89d45b5df988cb57cf63d2488da4e6c269
Brian Jordan <bcjordan@gmail.com>
2012-10-29 09:41:32 -0700

    extra stuff

--- ex12-extra.c
+++ ex12-extra.c
@@ -5,18 +5,36 @@ int main(int argc, char *argv[])
   int i = 0;
 
   if(argc == 1) {
-    printf("You only have one argument. You suck.\n");
-  } else if(argc > 1 || argc < 4) {
-    printf("Here's your arguments:\n");
+    printf("You have no arguments. You suck.\n");
+  } else if(argc > 1 && argc < 4) {
+    printf("Here's your %d arguments:\n", argc - 1);
 
     for(i = 0; i < argc; i++) {
       printf("%s ", argv[i]);
     }
-    printf("\n");
+  } else if(argc == 4) {
+    printf("Nice! %d arguments!", argc);
   } else {
-    printf("You have too many arguments. You suck.\n");
+    printf("You have too many arguments. You suck.");
   }
 
+  printf("\n");
   return 0;
 }
+
+/**
+ * Other boolean operators:
+ *
+ * http://en.wikipedia.org/wiki/Boolean_operation
+ *
+ * http://en.wikipedia.org/wiki/Operators_in_C_and_C%2B%2B#Logical_operators
+ *
+ * !, &&, ||
+ * ~ bitwise NOT
+ * & bitwise AND
+ * | bitwise OR
+ * ^ bitwise XOR
+ * << bitwise lshift
+ * >> bitwise rshift
+ */
 
